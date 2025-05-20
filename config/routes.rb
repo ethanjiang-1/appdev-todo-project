89682bf1afcf684e0443d34ba8b1726a7f194027
ethanjiang <ethanjiang@uchicago.edu>
2025-05-20 16:33:13 -0500

    sign in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,16 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  
+
+  # personal tasks
+  resources :tasks, only: %i[index new create edit update destroy]  
+
+  # group‐scoped tasks + member management
+  resources :groups do
+    resources :group_memberships, only: %i[index new create edit update destroy]
+    resources :tasks, only: %i[index new create edit update destroy], module: :groups
+  end
+
+  root "tasks#index"
+
 end
